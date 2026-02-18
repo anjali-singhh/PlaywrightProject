df623bcbf0d3b62ef12939a3fc9388815d07b8e9
anjali-singh <12anjali.singh21@gmail.com>
2026-02-18 18:05:48 +0530

    first commit

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,29 @@
+package base;
+
+import org.testng.annotations.BeforeMethod;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class BaseTest {
+	protected Playwright playwright;
+	protected Browser browser;
+	protected Page page;
+
+	@BeforeMethod
+	public void setup() {
+		playwright = Playwright.create();
+		browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(1000));
+		page = browser.newPage();
+	}
+
+	public void teardown() {
+		if (browser != null)
+			browser.close();
+		if (playwright != null)
+			playwright.close();
+
+	}
+}
---
+++ src/test/java/tests/FirstTest.java
@@ -0,0 +1,30 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import base.BaseTest;
+
+@Test
+public class FirstTest extends BaseTest {
+	public void verifyTitle()  {
+		page.navigate("https://smartcode.cmsuat.co.in/esamudra/login/ncr");
+		if(page.isVisible("button:has-text('Accept all')")) {
+			page.click("button:has-text('Accept all')");
+		}
+		System.out.println("Page title is : " + page.title());
+	}
+
+//	public static void main(String[] args) {
+//
+//		try (Playwright playwright = Playwright.create()) {
+//			Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+//			Page page = browser.newPage();
+//			page.navigate("https://smartcode.cmsuat.co.in/esamudra/login");
+//
+//			System.out.println("Page title is : " + page.title());
+//			browser.close();
+//
+//		}
+	}
+
+
